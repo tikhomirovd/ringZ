7ba2ccf88c625c71d74f43d1bac54816d833da80
Dmitry Tikhomirov <dimasta00@gmail.com>
2018-10-22 16:24:05 +0300

    Add ring Z

---
+++ ringZ.py
@@ -0,0 +1,27 @@
+class Zn(object):
+
+    def __init__(self, n):
+        self.n = n
+
+    def modZ(self, x):
+        return x % self.n
+
+    def sumZ(self, x, y):
+        return self.modZ(x + y)
+
+    def subZ(self, x, y):
+        return self.modZ(x - y)
+
+    def multZ(self, x, y):
+        return self.modZ(x * y)
+
+    def search_inverse(self, x, n=None, s=1, t=0, N=0):
+        if n is None: n = self.n
+        MMI = lambda x, n, s=1, t=0, N=0: (n < 2 and t % N or MMI(n, x % n, t, s - x // n * t, N or n), -1)[n < 1]
+        return MMI(n, x % n, t, s - x // n * t, N or n)
+
+    def div(self, x, y):
+        if self.search_inverse(y) != -1:
+            return self.modZ(x * self.search_inverse(y))
+        else:
+            return -1
